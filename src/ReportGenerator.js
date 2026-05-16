74e0b444983405629517010063ae6b248fe016fc
raphael sena <rsenares1@gmail.com>
2026-05-16 15:07:46 -0300

    fix: fixes test build

--- src/ReportGenerator.js
+++ src/ReportGenerator.js
@@ -37,7 +37,9 @@ export class ReportGenerator {
           total += item.value;
         } else if (reportType === 'HTML') {
           const style = item.priority ? 'style="font-weight:bold;"' : '';
-          report += `<tr ${style}><td>${item.id}</td><td>${item.name}</td><td>${item.value}</td></tr>\n`;
+          report += style
+            ? `<tr ${style}><td>${item.id}</td><td>${item.name}</td><td>${item.value}</td></tr>\n`
+            : `<tr><td>${item.id}</td><td>${item.name}</td><td>${item.value}</td></tr>\n`;
           total += item.value;
         }
       } else if (user.role === 'USER') {
